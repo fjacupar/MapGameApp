199eb84845f3efe6755ba73d9b8a888a1596b6d3
Fran Acuña <fjacuna@emergya.com>
2020-09-16 15:48:29 +0200

    Comments and renaming code/classes

--- src/app/constants/endpoints.ts
+++
@@ -1,5 +0,0 @@
-export class Endpoints {
-
-    public static WORLD_COUNTRIES_FILE ='assets/world_Countries.geojson';
-    public static NATO_COUNTRIES_FILE ='assets/nato_countries.json';
- }
---
+++ src/app/constants/paths.ts
@@ -0,0 +1,7 @@
+export class Paths {
+
+    public static WORLD_COUNTRIES_FILE ='assets/world_Countries.geojson';
+    public static NATO_COUNTRIES_FILE ='assets/nato_countries.json';
+    public static CLOSE_PANEL_ICON_URL = "assets/images/close-panel.png";
+    public static OPEN_PANEL_ICON_URL = "assets/images/open-panel.png";
+ }
